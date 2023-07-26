d1c48cdfa5106c8f2bdbc9a6eaec4dd5f7688f58
Er Aamir Bashir <ksaak33@gmail.com>
2023-07-27 04:33:31 +0530

    New Document root + using env file and

--- application/config/config.php
+++ application/config/config.php
@@ -393,7 +393,7 @@ $config['sess_expiration'] = 7200;
 $config['sess_match_ip'] = FALSE;
 // $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
-$config['sess_driver'] = 'rep_db'; // Use the database session driver
+$config['sess_driver'] = 'database'; // Use the database session driver
 $config['sess_save_path'] = 'ci_sessions'; // Table name to store sessions
 $config['sess_time_to_update'] = 2000; // Regenerate session ID every 5 minutes
 
